cc2c883d45fac6802998e8d1b97a424d0d7f50d0
raj4shr <118330943+raj4shr@users.noreply.github.com>
2023-01-23 16:11:30 +1300

    MVP Competition task using NUnit,POM,ExcelDataReader,Fluent Assertions, Data Driven Testing using TestCase atribute and Extent Reports

--- MVPCompetitionTask/Pages/PortalLoginPages/LoginToPortalPage.cs
+++ MVPCompetitionTask/Pages/PortalLoginPages/LoginToPortalPage.cs
@@ -1,6 +1,8 @@
 ﻿using AventStack.ExtentReports;
+using ExcelDataReader;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,12 @@ public class LoginToPortalPage : CommonDriver,IExtentRpt
     private CommonSendKeysAndClickElements findElements;
     private ExtentTest test;
     private ExtentReports testReport;
+    private IExcelDataReader reader;
+    private FileStream fileStream;
+    private DataSet dataSet;
+    private DataTable dataTable;
+    private string filePath,userName,password;
+
     
     public LoginToPortalPage()
     {
@@ -35,17 +43,33 @@ public class LoginToPortalPage : CommonDriver,IExtentRpt
     }
 
     public void EnterLoginDetails()
-    {
+    {   
         try
         {
+            filePath = @"C:\MVPCompetionTask2NUnit\MVPCompetitionTaskNUnit\MVPCompetitionTask\LoginCred.xlsx";
+            //Encoding excel file stream
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            //Reading login credentials from excel file to be used in login page
+            fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            reader=ExcelReaderFactory.CreateOpenXmlReader(fileStream);
+            dataSet=reader.AsDataSet();
+            dataTable=dataSet.Tables[0];
+            userName = dataTable.Rows[1][0].ToString();
+            password = dataTable.Rows[1][1].ToString();
+
             findElements.TakeScreenShot();
             //Finding and interacting with elements using my custom common methods
-            findElements.sendKeysToElement(nameof(By.Name), "email", "raj4shr@gmail.com");
-            findElements.sendKeysToElement(nameof(By.Name), "password", "IndustryConnect2023");
+            findElements.sendKeysToElement(nameof(By.Name), "email", userName);
+            findElements.sendKeysToElement(nameof(By.Name), "password", password);
             test.Log(Status.Info, "Send username and password credentials");
             findElements.clickOnElement(nameof(By.XPath), "//button[text()='Login']");
             test.Log(Status.Info, "Login Button Clicked");
             test.Log(Status.Pass, "Test Passed");
+
+            //Closing file streams
+            fileStream.Close();
+            reader.Close();
         }
         catch
         {
